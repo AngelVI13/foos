d6d6922ad48ccd8616d28e80d9cc75fc2e95e0a3
AngelVI <a.v.iliev13@gmail.com>
2023-11-13 15:57:04 +0200

    fix team ordering issue

--- game/teams.go
+++ game/teams.go
@@ -10,7 +10,24 @@ import (
 	"github.com/mroth/weightedrand/v2"
 )
 
-func saveTeamsToFile(teams map[string]string, newTeamsName, lastTeamsName string) error {
+type rawTeams [][2]string
+
+func (t rawTeams) Partner(player string) string {
+	for _, team := range t {
+		for i, p := range team {
+			if p == player {
+				if i == 0 {
+					return team[1]
+				} else {
+					return team[0]
+				}
+			}
+		}
+	}
+	return ""
+}
+
+func saveTeamsToFile(teams rawTeams, newTeamsName, lastTeamsName string) error {
 	// delete existing teams
 	os.Rename(newTeamsName, lastTeamsName)
 
@@ -22,16 +39,16 @@ func saveTeamsToFile(teams map[string]string, newTeamsName, lastTeamsName string
 	return os.WriteFile(newTeamsName, b, 0o666)
 }
 
-func loadTeamsFromFile(name string) map[string]string {
+func loadTeamsFromFile(name string) rawTeams {
 	b, err := os.ReadFile(name)
 	if err != nil {
-		return map[string]string{}
+		return rawTeams{}
 	}
 
-	var teams map[string]string
+	var teams rawTeams
 	err = json.Unmarshal(b, &teams)
 	if err != nil {
-		return map[string]string{}
+		return rawTeams{}
 	}
 
 	return teams
@@ -39,15 +56,20 @@ func loadTeamsFromFile(name string) map[string]string {
 
 func generateTeams(
 	players []string,
-	prevTeams1, prevTeams2 map[string]string,
-) map[string]string {
-	teams := map[string]string{}
+	prevTeams1, prevTeams2 rawTeams,
+) [][2]string {
+	teams := [][2]string{} // use this in order to keep teams order
+	for len(teams) < len(players)/2 {
+		teams = append(teams, [2]string{})
+	}
+	log.L.Info("", "teams", teams)
+	currentTeamIndex := 0
 
 	var player string
 	for len(players) > 1 {
 		player, players = playersPop(players)
-		prevPartner1 := prevTeams1[player]
-		prevPartner2 := prevTeams2[player]
+		prevPartner1 := prevTeams1.Partner(player)
+		prevPartner2 := prevTeams2.Partner(player)
 		log.L.Info(fmt.Sprintf("Selecting partner for %s\n", player))
 		log.L.Info(fmt.Sprintf("Last partners: %s %s\n", prevPartner1, prevPartner2))
 
@@ -69,8 +91,9 @@ func generateTeams(
 		result := chooser.Pick()
 		players = playersRemove(players, result)
 
-		log.L.Info("", player, result)
-		teams[player] = result
+		teams[currentTeamIndex][0] = player
+		teams[currentTeamIndex][1] = result
+		currentTeamIndex++
 	}
 	return teams
 }
@@ -130,17 +153,17 @@ func GenerateTeams(players []string) ([]Team, error) {
 	log.L.Info("", "Previous Teams1:", prevTeams1)
 	log.L.Info("", "Previous Teams2:", prevTeams2)
 
-	teamsMap := generateTeams(players, prevTeams1, prevTeams2)
-	log.L.Info("", "Teams:", teamsMap)
+	teamsSlice := generateTeams(players, prevTeams1, prevTeams2)
+	log.L.Info("", "Teams:", teamsSlice)
 
-	err := saveTeamsToFile(teamsMap, TeamsFile1, TeamsFile2)
+	err := saveTeamsToFile(teamsSlice, TeamsFile1, TeamsFile2)
 	if err != nil {
 		return nil, err
 	}
 
 	var teams []Team
-	for p1, p2 := range teamsMap {
-		teams = append(teams, NewTeam(p1, p2))
+	for _, players := range teamsSlice {
+		teams = append(teams, NewTeam(players[0], players[1]))
 	}
 
 	return teams, nil
